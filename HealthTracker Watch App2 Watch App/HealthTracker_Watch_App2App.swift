8600f72fe5df85f37fd55917564d60ccb23b0e7c
ReggieGrant <grantreggie09@gmail.com>
2026-06-07 10:36:18 -0700

    Initial Commit

---
+++ HealthTracker Watch App2 Watch App/HealthTracker_Watch_App2App.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTracker_Watch_App2App.swift
+//  HealthTracker Watch App2 Watch App
+//
+//  Created by Reginald Grant on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthTracker_Watch_App2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
